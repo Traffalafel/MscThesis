c91d731e3f8744845180d9e435a0f69b228839d9
Thomas Träff <thomas@traff.dk>
2022-11-10 10:12:20 +0100

    Fix error in script and add more specs

--- create_and_copy_all.ps1
+++ create_and_copy_all.ps1
@@ -1,28 +1,40 @@
 
-if ($args.Count -ne 3) {
-    Write-Output "Usage: <specs_dir_path> <output_dir_path> <template_file_path>"
-    return
-}
+# Script for generating HPC run-scripts from specifications and copying these to 
+# the remote HPC-server using ssh
 
-$specs_dir_path = $args[0]
-$scripts_dir_path = $args[1]
-$template_file_path = $args[2]
+$SPECS_DIR_PATH = "Specifications"
+$SCRIPTS_DIR_PATH = "hpc_scripts"
+$TEMPLATE_FILE_PATH = "run_script_template.txt"
+$RESULTS_DIR_PATH = "results"
 
 # clear local scripts folder
-Get-ChildItem $scripts_dir_path | ForEach-Object { $_.Delete() }
+Get-ChildItem $SCRIPTS_DIR_PATH | ForEach-Object { $_.Delete() }
 
-$template = Get-Content $template_file_path
+$template = Get-Content $TEMPLATE_FILE_PATH
 
-Get-ChildItem $specs_dir_path -Filter "*.json" | ForEach-Object {
+Get-ChildItem $SPECS_DIR_PATH -Filter "*.json" | ForEach-Object {
+    
     $name = $_.BaseName
+    
+    $results_file = $name + ".txt"
+    $results_path = Join-Path $RESULTS_DIR_PATH $results_file
+    if (Test-Path $results_path -PathType Leaf) {
+        Write-Output "Skipping ${name}"
+        return # skip if results file already exists
+    }
+    
+    Write-Output "Creating script for ${name}"
     $file_name = $name + ".sh"
-    $file_path = Join-Path $scripts_dir_path $file_name
+    $file_path = Join-Path $SCRIPTS_DIR_PATH $file_name
     $contents = $template.Replace("{name}", $name)
     $contents | Out-File $file_path
 }
 
-# delte remote scripts folder
-ssh -i ~/Desktop/.ssh/gbar s210520@login.hpc.dtu.dk 'rm -r -f hpc_scripts; rm -r -f v1_MscThesis/Specifications'
+# delete remote scripts folder
+ssh -i ~/Desktop/.ssh/gbar s210520@login.hpc.dtu.dk 'rm -r -f hpc_scripts'
 
+# copy scripts folder to remote
 scp -r -i ~/Desktop/.ssh/gbar hpc_scripts s210520@login.hpc.dtu.dk:hpc_scripts
-scp -r -i ~/Desktop/.ssh/gbar $specs_dir_path s210520@login.hpc.dtu.dk:v1_MscThesis/Specifications
+
+# copy specifications to remote
+scp -r -i ~/Desktop/.ssh/gbar $SPECS_DIR_PATH s210520@login.hpc.dtu.dk:.
