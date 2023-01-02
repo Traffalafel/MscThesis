ebbb3933457934fc67decc9f2098a5763c354ee2
Thomas Träff <thomas@traff.dk>
2023-01-02 16:23:16 +0100

    adios

--- create_and_copy_all.ps1
+++ create_and_copy_all.ps1
@@ -5,21 +5,21 @@
 $DTU_USER_ID = "s210520"
 $SSH_KEYS_DIR_PATH = "~/Desktop/.ssh/gbar"
 $SPECS_DIR_PATH = "Specifications"
+$SPECS_DIR_TMP_PATH = "specs_tmp"
 $SCRIPTS_DIR_PATH = "hpc_scripts"
 $TEMPLATE_FILE_PATH = "run_script_template.txt"
 $RESULTS_DIR_PATH = "results"
 
-if (Test-Path $RESULTS_DIR_PATH) {
-    # clear local scripts folder
-    Get-ChildItem $SCRIPTS_DIR_PATH | ForEach-Object { $_.Delete() }
+if (Test-Path $SCRIPTS_DIR_PATH) {
+    Remove-Item $SCRIPTS_DIR_PATH -Recurse -Force
 }
-else {
-    # create local scripts folder if missing
-    New-Item $SCRIPTS_DIR_PATH -ItemType Directory
+if (Test-Path $SPECS_DIR_TMP_PATH) {
+    Remove-Item $SPECS_DIR_TMP_PATH -Recurse -Force
 }
+mkdir $SCRIPTS_DIR_PATH
+mkdir $SPECS_DIR_TMP_PATH
 
 $template = Get-Content $TEMPLATE_FILE_PATH
-
 $results_names = Get-ChildItem $RESULTS_DIR_PATH -Recurse -Filter "*.txt" | %{ @{Name=$_.BaseName} }
 
 Get-ChildItem $SPECS_DIR_PATH -Recurse -Filter "*.json" | ForEach-Object {
@@ -36,6 +36,8 @@ Get-ChildItem $SPECS_DIR_PATH -Recurse -Filter "*.json" | ForEach-Object {
     $file_path = Join-Path $SCRIPTS_DIR_PATH $file_name
     $contents = $template.Replace("{name}", $name)
     $contents | Out-File $file_path
+
+    Copy-Item $_ $SPECS_DIR_TMP_PATH
 }
 
 # delete remote scripts folder
@@ -45,4 +47,4 @@ ssh -i $SSH_KEYS_DIR_PATH "${DTU_USER_ID}@login.hpc.dtu.dk" "rm -r -f hpc_script
 scp -r -i $SSH_KEYS_DIR_PATH $SCRIPTS_DIR_PATH "${DTU_USER_ID}@login.hpc.dtu.dk:hpc_scripts"
 
 # copy specifications to remote
-scp -i $SSH_KEYS_DIR_PATH $SPECS_DIR_PATH/**/*.json "${DTU_USER_ID}@login.hpc.dtu.dk:Specifications"
+scp -i $SSH_KEYS_DIR_PATH $SPECS_DIR_TMP_PATH/*.json "${DTU_USER_ID}@login.hpc.dtu.dk:Specifications"
